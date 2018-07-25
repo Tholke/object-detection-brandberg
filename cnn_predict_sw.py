77c59a0b4c593d18ff2f6fb778075b9c4632f322
dendelyne <tarek.khellaf@gmail.com>
2018-07-25 17:25:40 +0200

    Update cnn_predict_sw.py

--- cnn_predict_sw.py
+++ cnn_predict_sw.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from helpers import pyramid
 from helpers import sliding_window
+from helpers import intersection
 import time
 import cv2
 import os
@@ -41,7 +42,7 @@ def cnn_model(features, labels, mode):
 
         
 def main(unused_argv):
-    saver = tf.train.import_meta_graph('output/brandberg_cnn/model.ckpt-2570.meta')
+    saver = tf.train.import_meta_graph('output/brandberg_cnn/model.ckpt-3250.meta')
 
     with tf.Session() as sess:
         saver.restore(sess, tf.train.latest_checkpoint('output/brandberg_cnn/'))
@@ -50,66 +51,107 @@ def main(unused_argv):
     
         # load the image and define the window width and height
         image = cv2.imread('data/images/BOOK-0824731-0059.jpg')
-#         image = cv2.imread('testjpeg.jpg')
         (winW, winH) = (224, 224)
         
         imageList = []
+        debug_list = []
+        predictionsForSegmentation = []
         
         index = 0
-        # loop over the image pyramid
+        # Loopt über die Bilder verschiedener Größe
         for resized in pyramid(image, scale=1.5):
-            # loop over the sliding window for each layer of the pyramid
+            # Loopt über das Bild indem der momentan betrachtete Auschnitt immer wieder verschoben wird
             for (x, y, window) in sliding_window(resized, stepSize=30, windowSize=(winW, winH)):
-                # if the window does not meet our desired window size, ignore it
+                # Hat das Fenster nicht die richtige Größe wird es ignoriert
                 if window.shape[0] != winH or window.shape[1] != winW:
                     continue
                 
                 index = index + 1
-
-   
-                clone = resized.copy()
-                clone2 = resized.copy()
-                cv2.rectangle(clone2, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
                 
+                # Macht eine Kopie des Bildes
+                clone = resized.copy()
+                
+                # Schneidet den richtigen Auschnitt aus und bereitet es für die Eingabe in Tensorflow vor
                 cropped = clone[y :y + winW , x : x + winW]
-                cropped = cropped.astype(np.float32)
+                cropped = cropped.astype(np.float64)
                 images = []
                 images.append(cropped)
                 images = np.asarray(images)
-            
+                
+                #Das neuronale Netzwerk wird geladen
                 classifier = tf.estimator.Estimator(
                     model_fn=cnn_model, model_dir="output/brandberg_cnn/")
                
-                pred_input_fn = tf.estimator.inputs.numpy_input_fn(images, shuffle=False)
-        
-                classifier = tf.estimator.Estimator(
-                    model_fn=cnn_model, model_dir="output/brandberg_cnn/")
-            
+                #Regionen werden dem Netzwerk übergeben und es gibt Predictions aus
                 input_fn = tf.estimator.inputs.numpy_input_fn(
                     x={"x": images},
                     num_epochs=1,
                     shuffle=False)
             
                 predictions = classifier.predict(input_fn = input_fn)
+                #Predictions werden in eine Liste gespeichert
                 list_pred = list(predictions)
+        
+                for img in reversed(list_pred):
+                    probs = img.get('probabilities')
                 
-        #         captch_ex('out.png')
+                #Regionen, bei denen sich das Netzwerk zu 95% sicher ist, werden in ein Array gespeichert.
+                #D.h. alle Regionen, bei denen das Netzwerk ein Mensch oder ein Tier erkannt hat 
                 ind = len(list_pred)-1
                 for img in reversed(list_pred):
                     probs = img.get('probabilities')
-                    #print(probs)
-#                     print(str(probs[0]) + "   " + str(probs[1]) + "   " + str(probs[2]))
-                    if probs[0] > 0.8 or probs[1] > 0.8:
+                    if probs[0] > 0.95 or probs[1] > 0.95:
                         imageList.append([probs[0], probs[1], probs[2], cropped, x, y, winW, winH])
-                        print(imageList[-1])
+                        predictionsForSegmentation.append(max(probs[0], probs[1]))
+                        debug_list.append([x, y, x+winW, y+winH])
+#                         print(imageList[-1])
+#                         print('Länge:', len(imageList))
                     ind -= 1
         
-                cv2.imshow('Window', images[0])
-                cv2.waitKey(1)
-                time.sleep(0.025)
+    i = 0
+    arraylen = len(debug_list)
+    print('Vorher:', arrayLen)
     
+    #Durchläuft alle identifizierten Menschen und Tiere und filtert identische Objekte heraus
+    #Sind zwei identische Objekte gefunden, wird das mit der niedrigeren Wahrscheinlichkeit gelöscht
+    while i < arraylen:
+        j = i+1
+        while j < arraylen:
+            if intersection(debug_list[i], debug_list[j]):
+                #Die Wahrscheinlichkeit, mit der das neuronale Netz ein Tier oder Mensch erkannt hat
+                probsImgA = predictionsForSegmentation[i]
+                probsImgB = predictionsForSegmentation[j]
+      
+                if probsImgB <= probsImgA:
+                    imagesForSegmentation.pop(j)
+                    debug_list.pop(j)
+                    predictionsForSegmentation.pop(j)
+                    j -= 1
+                    arraylen -= 1
+                else:
+                    imageList.pop(i)
+                    debug_list.pop(i)
+                    predictionsForSegmentation.pop(i)
+                    i -= 1
+                    arraylen -= 1
+                    break
+            j += 1
+        i += 1
+            
+    
+    print('Nachher:', arrayLen)
+    
+    #Pfad unter dem die Segmentierten bilder abgespeichert werden können
+    savePath = 'output/'
+    
+    #Restliche Regionen aus dem zu segmentierenden Array werden durch den Watershed-Algorithmus segmentiert
+    for l in imagesList:
+        saveImagePath = savePath + imagePath[:-4] + '-' + str(i) + '.jpg'
+        watershed.computeWatershed(l[3], saveImagePath)
 
-        print(imageList)        
+                
+                
+    print(imageList)        
 
 if __name__ == "__main__":
     tf.app.run()
