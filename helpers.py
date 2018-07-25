ac9f9f2cf7eaa757f050dd1acf0d8dc795689786
dendelyne <tarek.khellaf@gmail.com>
2018-07-25 08:48:02 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -56,3 +56,21 @@ def sliding_window(image, stepSize, windowSize):
 		for x in range(0, image.shape[1], stepSize):
 			# yield the current window
 			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+			
+def intersection(boxA, boxB):
+	interXmin = max(boxA[0], boxB[0])
+	interYmin = max(boxA[1], boxB[1])
+	interXmax = min(boxA[2], boxB[2])
+	interYmax = min(boxA[3], boxB[3])
+
+	interArea = max(0, interXmax - interXmin) * max(0, interYmax - interYmin)
+
+	boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+	boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
+
+	ioboxA = interArea / boxAArea
+	ioboxB = interArea / boxBArea
+
+	minInter = min(ioboxA, ioboxB)
+
+	return (ioboxA > 0.8 or ioboxB > 0.8) and minInter > 0.5
